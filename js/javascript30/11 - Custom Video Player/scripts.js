b11c92e45377800eb7c209db8d17cb249a5a55cb
sabertazimi <sabertazimi@gmail.com>
2018-07-23 16:16:59 +0800

    add(js30-day11): use flex-basis and video.buffered to dispaly progres

--- js/javascript30/11 - Custom Video Player/scripts.js
+++ js/javascript30/11 - Custom Video Player/scripts.js
@@ -2,12 +2,17 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
+const bufferBar = player.querySelector('.progress__buffered');
 const toggleButton = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 const isBuffered = (time) => {
-    return (time < video.buffered.end(0));
+    if (video.buffered.length > 0) {
+        return (time < video.buffered.end(0));
+    }
+
+    return false;
 };
 
 const onToggle = function(event) {
@@ -33,23 +38,23 @@ const onProgress = function(event) {
     const percent = (video.currentTime / video.duration) * 100;
     progressBar.style.flexBasis = `${percent}%`;
 
-    if (video.buffered.length === 1) {
-        const buf = video.buffered;
-        console.group('Buffer Time');
-        console.log(buf.end(0));
-        console.log(video.duration);
-        console.groupEnd();
+    if (video.buffered.length > 0) {
+        const bufferedPercent = (video.buffered.end(0) - video.currentTime) / video.duration * 100;
+        bufferBar.style.flexBasis = `${bufferedPercent}%`;
     }
 };
 
 const onScrub = function(event) {
-    const scrubTime = (event.offsetX / progress.offsetWidth) * video.duration;
+    // fix offset when clicking on buffered bar
+    const offset = event.target.classList.contains('progress__buffered') ? progressBar.offsetWidth : 0;
+    const scrubTime = (event.offsetX + offset) / progress.offsetWidth * video.duration;
     isBuffered(scrubTime) && (video.currentTime = scrubTime);
 };
 
 video.addEventListener('click', onToggle);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('canplay', onProgress);
 video.addEventListener('timeupdate', onProgress);
 
 let mousedown = false;
