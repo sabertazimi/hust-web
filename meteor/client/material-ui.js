c59bed40a922c4b1c67f20edb7b6443be5a6bf83
sabertazimi <sabertazimi@gmail.com>
2018-07-26 16:21:43 +0800

    remove(backend): purge useless backend demo

--- meteor/client/material-ui.js
+++
@@ -1,4 +0,0 @@
-/*
- * Created by hustlyl on 16-1-8.
- */
-$.material.init();
--- meteor/server/startup.js
+++
@@ -1,5 +0,0 @@
-Meteor.startup(function () {
-    if(Documents.find().count() == 0) {
-        Documents.insert({title:"My First Document"});
-    }
-});
