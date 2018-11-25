b25f78986aeee86ef0d47d7b8ba36d664cf991b5
sabertazimi <sabertazimi@gmail.com>
2018-11-25 20:51:14 +0800

    update(ife-mis): merge same product to single `td`
    issue sabertazimi/awesome-notes#105

--- ife/2018/basic/mis/src/components/table.js
+++ ife/2018/basic/mis/src/components/table.js
@@ -16,11 +16,8 @@ const generateRow = ({
   product,
   region,
   sales,
-}) => {
-  // const { mixedFilters } = getState();
-  // console.log(mixedFilters);
-
-  const productHTML = `<td>${product}</td>`;
+}, rowspan = 1) => {
+  const productHTML = (rowspan === 0) ? '' : `<td rowspan="${rowspan}">${product}</td>`;
   const regionHTML = `<td>${region}</td>`;
   const salesHTML = sales.map(sale => `<td class="right">${sale.toString(10).padStart(3)}</td>`).join('');
 
@@ -34,9 +31,26 @@ const generateRow = ({
 };
 
 const generateTable = (data) => {
+  const sortedData = data.sort((a, b) => {
+    if (a.product < b.product) return -1;
+
+    if (a.product === b.product) {
+      if (a.region < b.region) return -1;
+      if (a.region === b.region) return 0;
+    }
+
+    return 1;
+  });
   const months = [...Array.from(Array(12).keys())].map(number => (number + 1));
   const headersHTML = generateHeader(['product', 'region', ...months]);
-  const bodyHTML = data.map(item => generateRow(item)).join('');
+  const bodyHTML = sortedData.map((item, index, array) => {
+    if (index === 0 || array[index - 1].product !== item.product) {
+      const rowspan = array.reduce((acc, cur) => (acc + Number(item.product === cur.product)), 0);
+      return generateRow(item, rowspan);
+    }
+
+    return generateRow(item, 0);
+  }).join('');
 
   return (`
     ${headersHTML}
