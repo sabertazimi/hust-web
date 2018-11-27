262b2d7a1a40eca3b55daa0e5a1bee5534d89aac
sabertazimi <sabertazimi@gmail.com>
2018-11-27 23:05:08 +0800

    add(ife-oop): implement observer pattern
    issue sabertazimi/awesome-notes#105

---
+++ ife/2018/basic/oop/src/utils/emitter.js
@@ -0,0 +1,35 @@
+export default class Emitter {
+  constructor() {
+    this.channels = {
+      default: [],
+    };
+  }
+
+  /**
+   * @param {*} channel
+   * @param {*} receiver
+   * @returns unsubscribe index for receiver
+   * @memberof Emitter
+   */
+  on(channel, receiver) {
+    // create new channel
+    if (!this.channels[channel]) {
+      this.channels[channel] = [];
+    }
+
+    this.channels[channel].push(receiver);
+    return this.channels[channel].indexOf(receiver);
+  }
+
+  off(channel, index) {
+    this.channels[channel].splice(index, 1);
+  }
+
+  emit(channel, message) {
+    if (!this.channels[channel]) {
+      return;
+    }
+
+    this.channels[channel].forEach(receiver => receiver(message));
+  }
+}
--- ife/2018/basic/oop/src/utils/index.js
+++ ife/2018/basic/oop/src/utils/index.js
@@ -1,7 +1,11 @@
+import Emitter from './emitter';
+import Factory from './factory';
 import $log from './log';
 import sleep from './sleep';
 
 export {
+  Emitter,
+  Factory,
   $log,
   sleep,
 };
