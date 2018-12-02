a8fbe50a4f0122b0bc2aa87a1f15e983c14009ff
sabertazimi <sabertazimi@gmail.com>
2018-12-02 23:06:54 +0800

    update(ife-mvvm): create `Link` component for routing
    issue sabertazimi/awesome-notes#105

---
+++ ife/2018/mvvm/src/components/index.js
@@ -0,0 +1,7 @@
+import Link from './Link.san';
+import Task1 from './Task1.san';
+
+export {
+  Link,
+  Task1,
+};
--- ife/2018/mvvm/src/index.js
+++ ife/2018/mvvm/src/index.js
@@ -1,10 +1,10 @@
-import {
-  router,
-} from 'san-router';
+import { router } from 'san-router';
 
 import App from './App.san';
+import Task1 from './components/Task1.san';
 
 import './index.css';
 
 router.add({ rule: '/', Component: App, target: '#app' });
+router.add({ rule: '/task1', Component: Task1, target: '#app' });
 router.start();
