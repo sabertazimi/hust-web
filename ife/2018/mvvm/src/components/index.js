81c72aa399a4d30587e5026bb49159e527a80174
sabertazimi <sabertazimi@gmail.com>
2018-12-16 01:08:55 +0800

    add(ife-mvvm): start task 2
    issue sabertazimi/awesome-notes#105

--- ife/2018/mvvm/src/components/index.js
+++ ife/2018/mvvm/src/components/index.js
@@ -1,7 +1,9 @@
 import Link from './Link.san';
 import Task1 from './Task1.san';
+import Task2 from './Task2.san';
 
 export {
   Link,
   Task1,
+  Task2,
 };
--- ife/2018/mvvm/src/index.js
+++ ife/2018/mvvm/src/index.js
@@ -3,10 +3,12 @@ import { router } from 'san-router';
 import App from './App.san';
 import {
   Task1,
+  Task2,
 } from './components';
 
 import './index.css';
 
 router.add({ rule: '/', Component: App, target: '#app' });
 router.add({ rule: '/task1', Component: Task1, target: '#app' });
+router.add({ rule: '/task2', Component: Task2, target: '#app' });
 router.start();
