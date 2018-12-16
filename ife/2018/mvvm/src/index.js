aa3664beb1c4503716a559aaa63d56af596f9a1b
sabertazimi <sabertazimi@gmail.com>
2018-12-16 15:01:31 +0800

    update(ife-mvvm): auto generate Task router
    issue sabertazimi/awesome-notes#105

--- ife/2018/mvvm/src/index.js
+++ ife/2018/mvvm/src/index.js
@@ -1,14 +1,15 @@
 import { router } from 'san-router';
 
 import App from './App.san';
-import {
-  Task1,
-  Task2,
-} from './components';
+import * as Tasks from './components/task';
 
 import './index.css';
 
 router.add({ rule: '/', Component: App, target: '#app' });
-router.add({ rule: '/task1', Component: Task1, target: '#app' });
-router.add({ rule: '/task2', Component: Task2, target: '#app' });
+
+Object.keys(Tasks).forEach((task) => {
+  const Task = Tasks[task];
+  router.add({ rule: `/${task}`, Component: Task, target: '#app' });
+});
+
 router.start();
