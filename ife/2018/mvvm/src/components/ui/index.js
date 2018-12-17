377d2437cb942fb3fb4ddfa85263e233eb388cb0
sabertazimi <sabertazimi@gmail.com>
2018-12-18 00:32:19 +0800

    update(ife-mvvm): task 10 - Calendar component
    issue sabertazimi/awesome-notes#105

--- ife/2018/mvvm/src/components/ui/index.js
+++ ife/2018/mvvm/src/components/ui/index.js
@@ -1,14 +1,18 @@
+import Calendar from './Calendar.san';
 import Cascader from './Cascader.san';
 import Checkbox from './Checkbox.san';
 import Input from './Input.san';
+import Modal from './Modal.san';
 import Panel from './Panel.san';
 import Link from './Link.san';
 import Task from './Task.san';
 
 export {
+  Calendar,
   Cascader,
   Checkbox,
   Input,
+  Modal,
   Panel,
   Link,
   Task,
