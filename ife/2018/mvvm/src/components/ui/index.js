81804d0eac37aa617636539f26b8374f3ccb12b8
sabertazimi <sabertazimi@gmail.com>
2018-12-18 10:25:32 +0800

    update(ife-mvvm): complete task 10 - DatePicker component
    issue sabertazimi/awesome-notes#105

--- ife/2018/mvvm/src/components/ui/index.js
+++ ife/2018/mvvm/src/components/ui/index.js
@@ -1,8 +1,8 @@
 import Calendar from './Calendar.san';
 import Cascader from './Cascader.san';
 import Checkbox from './Checkbox.san';
+import DatePicker from './DatePicker.san';
 import Input from './Input.san';
-import Modal from './Modal.san';
 import Panel from './Panel.san';
 import Link from './Link.san';
 import Task from './Task.san';
@@ -11,8 +11,8 @@ export {
   Calendar,
   Cascader,
   Checkbox,
+  DatePicker,
   Input,
-  Modal,
   Panel,
   Link,
   Task,
