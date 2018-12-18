1500ec9158d14572c239881f8ba39d4c6c219567
sabertazimi <sabertazimi@gmail.com>
2018-12-18 11:22:01 +0800

    update(ife-mvvm): task 11 - setup Form component
    issue sabertazimi/awesome-notes#105

--- ife/2018/mvvm/src/components/ui/index.js
+++ ife/2018/mvvm/src/components/ui/index.js
@@ -1,16 +1,22 @@
+import Button from './Button.san';
 import Calendar from './Calendar.san';
 import Cascader from './Cascader.san';
 import Checkbox from './Checkbox.san';
 import DatePicker from './DatePicker.san';
+import Form from './Form.san';
+import FormItem from './FormItem.san';
 import Input from './Input.san';
 import Panel from './Panel.san';
 import Link from './Link.san';
 import Task from './Task.san';
 
 export {
+  Button,
   Calendar,
   Cascader,
   Checkbox,
+  Form,
+  FormItem,
   DatePicker,
   Input,
   Panel,
