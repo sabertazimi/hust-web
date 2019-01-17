90350d583efe6ec8bbb1e14c4768c66d05d3863a
sabertazimi <sabertazimi@gmail.com>
2019-01-17 17:30:36 +0800

    update(react-nextjs): webpack analyzer configuration
    issue sabertazimi/awesome-notes#105

--- react/nextjs/next.config.js
+++ react/nextjs/next.config.js
@@ -1,8 +1,22 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { ANALYZE } = process.env;
+
 module.exports = {
   exportPathMap: () => {
     return {
       '/': { page: '/' },
       '/about': { page: '/about' },
     };
+  },
+  webpack: (config) => {
+    if (ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: 8888,
+        openAnalyzer: true,
+      }));
+    }
+
+    return config;
   },
 };
