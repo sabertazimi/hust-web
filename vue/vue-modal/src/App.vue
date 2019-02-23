4aaf51b4f51b7e9c3e50552f904c0ab49661dde3
sabertazimi <sabertazimi@gmail.com>
2019-02-23 15:24:57 +0800

    fix(vue-modal): rectify baseUrl error of dist

--- vue/vue-modal/src/App.vue
+++ vue/vue-modal/src/App.vue
@@ -29,11 +29,14 @@ export default {
 };
 </script>
 <style lang="stylus">
+body {
+  margin: 0;
+}
+
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
   color #2c3e50
-  margin-top 60px
 </style>
