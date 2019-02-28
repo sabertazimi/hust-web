7cded9411a795a84e4a1891829dc9363a7a2317e
sabertazimi <sabertazimi@gmail.com>
2019-02-28 20:38:18 +0800

    update(css-slides): add concise control with JavaScript
    - concise link click control
    - concise left/right arrow control
    - reset scoll when slides change

--- css/target-slide/index.js
+++ css/target-slide/index.js
@@ -1,11 +1,76 @@
+const slideContainer = document.querySelector('.slide-container');
 const slides = document.querySelectorAll('.slide');
 const select = document.querySelector('.animation-select');
+
+// reset scrollX when change slides
+const resetScollX = () => {
+  window.scrollTo(0, 0);
+};
+
+// slide change utils function
+const getCurrentSlideIndex = () => {
+  const { hash } = window.location;
+  return Number.parseInt(hash.slice(hash.length - 1));
+};
+
+const getPrevSlideIndex = () => {
+  const currentIndex = getCurrentSlideIndex();
+  return currentIndex - 1 > 0 ? currentIndex - 1 : 1;
+};
+
+const getNextSlideIndex = () => {
+  const currentIndex = getCurrentSlideIndex();
+  return currentIndex + 1 < 6 ? currentIndex + 1 : 5;
+};
 
 const changeToSlide = (index) => {
   const { origin, pathname } = window.location;
   window.location = `${origin}${pathname}#slide${index}`;
-}
+  resetScollX();
+};
 
+const changeToPrevSlide = () => {
+  changeToSlide(getPrevSlideIndex());
+};
+
+const changeToNextSlide = () => {
+  changeToSlide(getNextSlideIndex());
+};
+
+// left or right arrow to browser slides
+window.addEventListener('keydown', (event) => {
+  const { key: keyName } = event;
+
+  event.preventDefault();
+  event.stopPropagation();
+
+  switch (keyName) {
+    case 'ArrowLeft':
+      changeToPrevSlide();
+      break;
+    case 'ArrowRight':
+      changeToNextSlide();
+      break;
+    default:
+      break;
+  }
+});
+
+// click prev/next slide link to browser slides
+slideContainer.addEventListener('click', (event) => {
+  if (event.target.matches('.slide-link')) {
+    event.preventDefault();
+    event.stopPropagation();
+    
+    if (event.target.matches('.slide-prev-link')) {
+      changeToPrevSlide();
+    } else {
+      changeToNextSlide();
+    }
+  }
+});
+
+// change slides animation
 select.addEventListener('change', (event) => {
   changeToSlide(1);
 
