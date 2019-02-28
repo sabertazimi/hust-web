64f255608ffd2f97a8bc1995392362e36570112e
sabertazimi <sabertazimi@gmail.com>
2019-02-28 19:06:47 +0800

    update(css-slide): switch to slide 1 initially

--- css/target-slide/index.js
+++ css/target-slide/index.js
@@ -1,10 +1,19 @@
 const slides = document.querySelectorAll('.slide');
 const select = document.querySelector('.animation-select');
 
+const changeToSlide = (index) => {
+  const { origin, pathname } = window.location;
+  window.location = `${origin}${pathname}#slide${index}`;
+}
+
 select.addEventListener('change', (event) => {
+  changeToSlide(1);
+
   slides.forEach((slide) => {
     slide.className = '';
     slide.classList.add('slide');
     slide.classList.add(`slide--${event.target.value}`);
   });
 });
+
+changeToSlide(1);
