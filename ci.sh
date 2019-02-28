fc74360993fdb1e07ea0d31c9ca0c723720c8def
sabertazimi <sabertazimi@gmail.com>
2019-02-28 11:12:48 +0800

    update(ci): rectify log error

--- ci.sh
+++ ci.sh
@@ -2,16 +2,16 @@
 
 echo "[JOBS] Start sub-projects building ..."
 
-echo "[JOB] Start `ife/2018/basic/mis` building ..."
+echo "[JOB] Start 'ife/2018/basic/mis' building ..."
 bash -c "cd ./ife/2018/basic/mis && npm install && npm run build && rm -fr src && mv ./build/* . && cd ../../../../" &
 
-echo "[JOB] Start `ife/2018/basic/oop` building ..."
+echo "[JOB] Start 'ife/2018/basic/oop' building ..."
 bash -c "cd ./ife/2018/basic/oop && npm install && npm run build && rm -fr src && mv ./build/* . && cd ../../../../" &
 
-echo "[JOB] Start `ife/2018/mvvm` building ..."
+echo "[JOB] Start 'ife/2018/mvvm' building ..."
 bash -c "cd ./ife/2018/mvvm && npm install && npm run build && rm -fr src && mv ./build/* . && cd ../../../" &
 
-echo "[JOB] Start `vue-modal` building ..."
+echo "[JOB] Start 'vue-modal' building ..."
 bash -c "cd ./vue/vue-modal && npm install && npm run build && rm -fr src && mv ./dist/* . && cd ../../" &
 
 wait
