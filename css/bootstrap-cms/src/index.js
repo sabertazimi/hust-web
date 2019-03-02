0d45e91e0882daf45e313be1b946da9b8cefc6ca
sabertazimi <sabertazimi@gmail.com>
2019-03-02 18:43:10 +0800

    update(bootstrap-CMS): complete editor and footer

--- css/bootstrap-cms/src/index.js
+++ css/bootstrap-cms/src/index.js
@@ -1,2 +1,14 @@
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 import './index.scss';
 import 'bootstrap';
+
+ClassicEditor
+  .create(document.querySelector('#pageEditor'), {
+    placeholder: 'Enter Page Content',
+  })
+  .catch(() => {
+    const ckeAlert = document.querySelector('#ckeAlert');
+    ckeAlert.classList.remove('d-none');
+    ckeAlert.classList.add('show');
+  });
