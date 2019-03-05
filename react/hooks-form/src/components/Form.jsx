1da6db65a993b8c2d09f5481e11279644b14961d
sabertazimi <sabertazimi@gmail.com>
2019-03-05 11:08:23 +0800

    add(react-hooks form): complete form based on hooks
    - React Hooks: useState
    - bulma

---
+++ react/hooks-form/src/components/Form.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import useForm from '../hooks/useForm';
+
+const Form = () => {
+  const login = () => {
+    console.log(values);
+    window.alert(JSON.stringify(values));
+  };
+
+  const { values, handleChange, handleSubmit } = useForm(login);
+
+  return (
+    <div className="section is-fullheight">
+      <div className="container">
+        <div className="column is-4 is-offset-4">
+          <div className="box">
+            <form onSubmit={handleSubmit}>
+              <div className="field">
+                <label className="label">Email Address</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Password</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="button is-block is-info is-fullwidth"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
