8002684b23cb295755c93f0a0e2172550fd83d2e
sabertazimi <sabertazimi@gmail.com>
2019-03-21 16:36:33 +0800

    update(angular-RxJS): change to async dataflow

--- angular/learn/src/app/hero.service.ts
+++ angular/learn/src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -8,7 +9,7 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
   constructor() {}
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero []> {
+    return of(HEROES);
   }
 }
