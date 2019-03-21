ea0bbbb1540730e62ce4394fbe40e634afa9b6c9
sabertazimi <sabertazimi@gmail.com>
2019-03-21 17:27:21 +0800

    update(angular-http): add http module

--- angular/learn/src/app/app.module.ts
+++ angular/learn/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,6 +21,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
