fa091b8331892b0a01bf42b837d24447b412eeb5
sabertazimi <sabertazimi@gmail.com>
2019-03-22 10:00:04 +0800

    update(angular-operators): debounceTime/distinctUntilChanged/switchMap

--- angular/learn/src/app/hero-search/hero-search.component.ts
+++ angular/learn/src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit() {
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
+  }
 }
