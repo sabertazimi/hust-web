4e2a69f74ea294da20251dfbf01100eb42e8f468
sabertazimi <sabertazimi@gmail.com>
2019-03-22 09:53:50 +0800

    update(angular-operators): create search component

--- angular/learn/src/app/hero.service.ts
+++ angular/learn/src/app/hero.service.ts
@@ -71,6 +71,17 @@ export class HeroService {
     return this.http.delete<Hero>(url, httpOptions).pipe(
       tap(_ => this.log(`deleted hero id=${id}.`)),
       catchError(this.handleError<Hero>('deleteHero'))
-    )
+    );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
   }
 }
