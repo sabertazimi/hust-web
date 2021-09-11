eb334d7d719c90344e75c12615ef34ee4ca369f5
sabertazimi <sabertazimi@gmail.com>
2021-09-12 00:06:16 +0800

    build(scripts): use `@lerna/version` package to release new version

--- scripts/release.js
+++ scripts/release.js
@@ -1,26 +1,46 @@
+const lernaVersion = require('@lerna/version');
 const semver = require('semver');
 const utils = require('./utils');
 
-const main = () => {
+const main = async () => {
+  utils.checkGitStatus();
   utils.info(`Working in directory ${process.cwd()}.`);
-  const args = process.argv.slice(2);
-  const versionMatch = utils
-    .execPipe(
-      'npx lerna version --force-publish --no-commit-hooks --no-git-tag-version --no-push --yes'
-    )
-    .toString()
-    .split('\n')
-    .join('')
-    .match(/=>\s\d+\.\d+\.\d+/i);
 
-  if (versionMatch) {
-    const version = semver.clean(versionMatch[0].replace('=> ', ''));
+  const opts = {
+    changelogPreset: 'angular',
+    conventionalCommits: true,
+    message: 'chore(release): %s',
+    forcePublish: true,
+    commitHooks: false,
+    gitTagVersion: false,
+    yes: true,
+  };
+
+  const push = process.argv.includes('-p') || process.argv.includes('--push');
+  const result = await lernaVersion({
+    ...opts,
+    push,
+  });
+
+  let proceed = false;
+  let version = '';
+
+  for (const v of result.updatesVersions.values()) {
+    if (semver.valid(v)) {
+      proceed = true;
+      version = semver.valid(v);
+      break;
+    }
+  }
+
+  if (proceed) {
     utils.exec('npm i');
+    utils.exec('git add .');
     utils.exec(`git commit -a -m "chore(release): ${version}"`);
     utils.exec(`git tag v${version} -s -m "v${version}"`);
 
-    if (utils.isFlag(args, '-p') || utils.isFlag(args, '--push')) {
-      utils.exec('git push --follow-tags');
+    if (push) {
+      utils.exec('git push --follow-tags origin main');
     } else {
       utils.info('Run `git push --follow-tags origin main` to publish.');
     }
--- scripts/utils.js
+++ scripts/utils.js
@@ -2,28 +2,39 @@ const chalk = require('chalk');
 const cp = require('child_process');
 const consola = require('consola');
 
-const log = (log) => {
+const log = log => {
   consola.log(log);
 };
 
-const info = (info) => {
+const info = info => {
   consola.info(chalk.blue(info));
 };
 
-const success = (success) => {
+const success = success => {
   consola.success(chalk.green(success));
 };
 
-const error = (error) => {
+const error = error => {
   consola.error(error);
 };
 
-const cmd = (cmd) => {
+const cmd = cmd => {
   console.info(`    ${chalk.bgGreen.black('[exec]')}: ${cmd}`);
 };
 
-const isFlag = (args, flag) =>
-  Boolean(args.length) && args.some((arg) => arg === flag);
+const checkGitStatus = () => {
+  info('Check git status ...');
+  const gitStatus = execPipe('git status --porcelain').toString();
+
+  if (gitStatus.trim() !== '') {
+    info('Please commit your changes before running this script!');
+    info('Exiting because `git status` is not empty:');
+    log('');
+    log(gitStatus);
+    log('');
+    process.exit(1);
+  }
+};
 
 const exec = (command, cwd) => {
   cmd(command);
@@ -43,4 +54,13 @@ const execPipe = (command, cwd) => {
   });
 };
 
-module.exports = { log, info, success, error, cmd, isFlag, exec, execPipe };
+module.exports = {
+  log,
+  info,
+  success,
+  error,
+  cmd,
+  checkGitStatus,
+  exec,
+  execPipe,
+};
