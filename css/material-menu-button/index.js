676c92d2ca62469dc7c0e3706977edd156973572
sabertazimi <sabertazimi@gmail.com>
2021-10-23 23:22:56 +0800

    style(css): format css code

--- css/material-menu-button/index.js
+++ css/material-menu-button/index.js
@@ -1,10 +1,13 @@
 const menu = document.querySelector('.menu');
-const menuButton = document.querySelector('.menu__button');
+const menuButton = document.querySelector('.menu-button');
 
 const toggleMenu = () => {
-  menu.classList.toggle('menu--open');
-  menuButton.setAttribute('aria-expanded', menuButton.getAttribute('aria-expanded') !== 'true')
-}
+  menu.classList.toggle('menu-open');
+  menuButton.setAttribute(
+    'aria-expanded',
+    menuButton.getAttribute('aria-expanded') !== 'true'
+  );
+};
 
 const toggleTimer = setInterval(() => {
   toggleMenu();
