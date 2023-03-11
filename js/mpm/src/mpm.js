276f0eacc9563b99404790a1adbde9dca54611be
sabertazimi <sabertazimi@gmail.com>
2023-03-11 10:23:37 +0800

    style(node-protocol): prefer `node:` protocol

--- js/mpm/src/mpm.js
+++ js/mpm/src/mpm.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('node:path');
 // const cp = require('child_process');
 // const util = require('util');
 // const exec = util.promisify(cp.exec);
