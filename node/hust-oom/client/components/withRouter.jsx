ba2fd929982b10cd8ee6bd6ce08f43817dd98bca
sabertazimi <sabertazimi@gmail.com>
2024-11-23 14:27:00 +0800

    fix(deps): change react-router-dom to react-router

--- node/hust-oom/client/components/withRouter.jsx
+++ node/hust-oom/client/components/withRouter.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router'
 
 function withRouter(Component) {
   function ComponentWithRouterProp(props) {
